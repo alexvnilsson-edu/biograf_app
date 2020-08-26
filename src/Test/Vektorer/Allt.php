efd610c287c55a9522d46ae74c99b0e0b8d06bbf
alexvnilsson <alexvnilsson@outlook.com>
2020-08-26 22:15:32 +0200

    Ta bort kodsnippets.

--- src/Test/Vektorer/Allt.php
+++
@@ -1,4 +0,0 @@
-<?php
-
-$pengar = array("Oscar"=>5000, "Bengt"=>10000, "Anna"=>15000);
-print_r($pengar);
--- src/Test/Vektorer/Enstaka.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-$pengar = array("Oscar"=>5000, "Bengt"=>10000, "Anna"=>15000);
-
-$vem = "Bengt";
-$vem_har = $pengar[$vem];
-echo("$vem har $vem_har kr." . PHP_EOL);
