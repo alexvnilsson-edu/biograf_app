aeea4418923f13b1bd9b4cb246f0a5fb92ee1b8d
alexvnilsson <alexvnilsson@outlook.com>
2020-08-27 16:17:48 +0200

    Uppdatera webpack-konfiguration.

--- webpack.config.js
+++ webpack.config.js
@@ -23,53 +23,23 @@ Encore
    * Each entry will result in one JavaScript file (e.g. app.js)
    * and one CSS file (e.g. app.css) if your JavaScript imports CSS.
    */
-  .addEntry("app", "./assets/js/app.js")
-  .addEntry(
-    "customer_registration",
-    "./assets/js/customer/customer-registration.js"
-  )
-
-  // When enabled, Webpack "splits" your files into smaller pieces for greater optimization.
-  .splitEntryChunks()
-
-  // will require an extra script tag for runtime.js
-  // but, you probably want this, unless you're building a single-page app
-  .enableSingleRuntimeChunk()
+  .addEntry("members_registration", "./assets/js/members/registration.js")
 
   /*
-   * FEATURE CONFIG
-   *
-   * Enable & configure other features below. For a full
-   * list of features, see:
-   * https://symfony.com/doc/current/frontend.html#adding-more-features
+   * Core
    */
+  .splitEntryChunks()
+  .enableSingleRuntimeChunk()
   .cleanupOutputBeforeBuild()
   .enableBuildNotifications()
-  .enableSourceMaps(!Encore.isProduction())
-  // enables hashed filenames (e.g. app.abc123.css)
+  //.enableSourceMaps(!Encore.isProduction())
   .enableVersioning(Encore.isProduction())
-
-  // enables @babel/preset-env polyfills
   .configureBabelPresetEnv((config) => {
     config.useBuiltIns = "usage";
     config.corejs = 3;
   })
-
-  // enables Sass/SCSS support
   .enableSassLoader()
-
-  // uncomment if you use TypeScript
-  //.enableTypeScriptLoader()
-
-  // uncomment to get integrity="..." attributes on your script & link tags
-  // requires WebpackEncoreBundle 1.4 or higher
   .enableIntegrityHashes(Encore.isProduction())
-
-  // uncomment if you're having problems with a jQuery plugin
-  //.autoProvidejQuery()
-
-  // uncomment if you use API Platform Admin (composer req api-admin)
   .enableReactPreset();
-//.addEntry('admin', './assets/js/admin.js')
 
 module.exports = Encore.getWebpackConfig();
