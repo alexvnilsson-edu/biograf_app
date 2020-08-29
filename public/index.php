de6db02a1a99efc610d1fffd47452de1da39305c
alexvnilsson <alexvnilsson@outlook.com>
2020-08-29 20:48:50 +0200

    Uppdatera server-konfig.

--- public/index.php
+++ public/index.php
@@ -25,6 +25,21 @@ if ($trustedHosts = $_SERVER['TRUSTED_HOSTS'] ?? false) {
 
 $kernel = new Kernel($_SERVER['APP_ENV'], (bool) $_SERVER['APP_DEBUG']);
 $request = Request::createFromGlobals();
+
+Request::setTrustedProxies(
+    // the IP address (or range) of your proxy
+    ['10.0.0.0/24'],
+
+    // trust *all* "X-Forwarded-*" headers
+    Request::HEADER_X_FORWARDED_ALL
+
+    // or, if your proxy instead uses the "Forwarded" header
+    // Request::HEADER_FORWARDED
+
+    // or, if you're using AWS ELB
+    // Request::HEADER_X_FORWARDED_AWS_ELB
+);
+
 $response = $kernel->handle($request);
 $response->send();
 $kernel->terminate($request, $response);
