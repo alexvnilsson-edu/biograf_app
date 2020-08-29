e8b365c07077df0dd47662ce639f3dce68a5824b
alexvnilsson <alexvnilsson@outlook.com>
2020-08-29 20:49:03 +0200

    Uppdatera registration.js.

--- assets/js/members/registration.js
+++ assets/js/members/registration.js
@@ -50,23 +50,24 @@ class CustomerRegistration extends Component {
   }
 
   handleFormSubmit(e) {
+    console.log("Skickar data till server:", this.state.form);
+
     axios({
       method: "post",
       url: `${window.APP_API_ENDPOINT}/api/medlemmar`,
       data: this.state.form,
     })
       .then((response) => {
-        console.log("OK!");
         this.setState({
           formSubmitted: true,
           formAccepted: true,
           member: response.data,
           form: CUSTOMER_FORM_DEFAULTS,
         });
-        console.log(this.state.member);
+        console.log("Svar från server:", this.state.member);
       })
       .catch((reason) => {
-        console.error(reason);
+        console.error("Felsvar från server:", reason);
         this.setState({ formSubmitted: true, formAccepted: false });
       });
 
@@ -96,7 +97,8 @@ class CustomerRegistration extends Component {
                   className="form-control"
                   name="email"
                   placeholder="E-postadress"
-                  value={this.state.email}
+                  required
+                  value={this.state.form.email}
                   onChange={this.handleFormChange}
                 />
               </div>
@@ -108,7 +110,8 @@ class CustomerRegistration extends Component {
                     className="form-control"
                     name="password"
                     placeholder="Lösenord"
-                    value={this.state.password}
+                    required
+                    value={this.state.form.password}
                     onChange={this.handleFormChange}
                   />
                 </div>
@@ -119,7 +122,8 @@ class CustomerRegistration extends Component {
                     className="form-control"
                     name="passwordConfirm"
                     placeholder="Bekräfta lösenord"
-                    value={this.state.passwordConfirm}
+                    required
+                    value={this.state.form.passwordConfirm}
                     onChange={this.handleFormChange}
                   />
                 </div>
@@ -136,7 +140,8 @@ class CustomerRegistration extends Component {
                     className="form-control"
                     name="firstname"
                     placeholder="Tilltalsnamn"
-                    value={this.state.firstname}
+                    required
+                    value={this.state.form.firstname}
                     onChange={this.handleFormChange}
                   />
                 </div>
@@ -147,7 +152,7 @@ class CustomerRegistration extends Component {
                     className="form-control"
                     name="surname"
                     placeholder="Efternamn"
-                    value={this.state.surname}
+                    value={this.state.form.surname}
                     onChange={this.handleFormChange}
                   />
                 </div>
@@ -163,12 +168,14 @@ class CustomerRegistration extends Component {
         </div>
       );
     } else if (this.state.formSubmitted && this.state.formAccepted) {
+      const fullName = `${this.state.member.firstname} ${
+        this.state.member.surname || ""
+      }`.trim();
+
       return (
         <div className="container my-5">
           <div className="container p-3 bg-light">
-            <h2>
-              Tack {this.state.member.firstname} {this.state.member.surname}!
-            </h2>
+            <h2>Tack {fullName}!</h2>
 
             <h4>Du är nu registrerad!</h4>
           </div>
