605258a87b6a57d7d75ef0e1f22df112c797072e
alexvnilsson <alexvnilsson@outlook.com>
2020-09-10 21:22:40 +0200

    Uppdatera UserType-formtyp.

--- src/Form/Type/UserType.php
+++ src/Form/Type/UserType.php
@@ -4,6 +4,7 @@ namespace App\Form\Type;
 
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\HiddenType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\Extension\Core\Type\PasswordType;
 use Symfony\Component\Form\FormBuilderInterface;
@@ -18,7 +19,7 @@ class UserType extends AbstractType
      *      message = "Angiven e-postadress ({{ value }}) är inte giltig."
      * )
      */
-    public $email;
+    protected $email;
 
     /**
      * @Assert\NotBlank
@@ -26,7 +27,7 @@ class UserType extends AbstractType
      *  max = 4096
      * )
      */
-    public $password;
+    protected $password;
 
     /**
      * @Assert\IdenticalTo(propertyPath="password")
@@ -34,18 +35,23 @@ class UserType extends AbstractType
      *  max = 4096
      * )
      */
-    public $passwordConfirm;
+    protected $passwordConfirm;
 
     /**
      * @Assert\NotBlank
      * @Assert\Type("string")
      */
-    public $firstname;
+    protected $firstname;
 
     /**
      * @Assert\Type("string")
      */
-    public $lastname;
+    protected $lastname;
+
+    /**
+     * @Assert\Type("string")
+     */
+    protected $_csrf_token;
 
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
@@ -54,6 +60,83 @@ class UserType extends AbstractType
             ->add("password", PasswordType::class)
             ->add("passwordConfirm", PasswordType::class)
             ->add("firstname", TextType::class)
-            ->add("lastname", TextType::class);
+            ->add("lastname", TextType::class)
+            ->add("_csrf_token", HiddenType::class);
+    }
+
+    //
+    // Getters and setters
+    //
+
+    public function getEmail()
+    {
+        return $this->email;
+    }
+    
+    public function setEmail($value): self
+    {
+        $this->email = $value;
+    
+        return $this;
+    }
+
+    public function getPassword()
+    {
+        return $this->password;
+    }
+    
+    public function setPassword($value): self
+    {
+        $this->password = $value;
+    
+        return $this;
+    }
+
+    public function getPasswordConfirm()
+    {
+        return $this->passwordConfirm;
+    }
+    
+    public function setPasswordConfirm($value): self
+    {
+        $this->passwordConfirm = $value;
+    
+        return $this;
+    }
+
+    public function getFirstname()
+    {
+        return $this->firstname;
+    }
+    
+    public function setFirstname($value): self
+    {
+        $this->firstname = $value;
+    
+        return $this;
+    }
+
+    public function getLastname()
+    {
+        return $this->lastname;
+    }
+    
+    public function setLastname($value): self
+    {
+        $this->lastname = $value;
+    
+        return $this;
+    }
+
+    public function getCsrfToken()
+    {
+        return $this->_csrf_token;
+    }
+    
+    public function setCsrfToken($value): self
+    {
+        $this->_csrf_token = $value;
+    
+        return $this;
     }
 }
